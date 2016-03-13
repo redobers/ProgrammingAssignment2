10782182754050ca3cf9b9e337da9ebafe17b5d1
redobers608 <redobers608@gmail.com>
2016-03-13 17:09:31 -0500

    completed assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,23 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Cache the inverse of a matrix
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## create square matrix of random numbers with dimensions defaulting to 9x9
 
+makeCacheMatrix <- function(x = matrix(), dimension = 9) {
+  x <<- NULL
+  x <<- matrix(runif((dimension*dimension), 1, 100), dimension, dimension)
+  print(x)
 }
 
 
-## Write a short comment describing this function
+## Return a matrix that is the inverse of x, either returning cached value
+## or if not, by calculating inverse
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  if(!is.null(x)) {
+    message("getting cached data")
+    return(x)
+  }
+  invx <- solve(x)
+  print(invx)
 }
